ac4451a4b223e5255f96aa5cf6644baf43324fb0
ezepsosa <ezequielperez50@gmail.com>
2024-10-01 16:59:51 +0200

    Refactor the routes of routing file

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./layout";
+import Home from "../pages/Home/Home";
 
 export function RoutesComponent() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
