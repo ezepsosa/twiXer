fb30e8ed87872c3c0ba12f6d8369449b1affa42b
ezepsosa <ezequielperez50@gmail.com>
2024-10-25 17:12:02 +0200

    [Backend] Refactor security to allow access to frontend

--- backend/api/src/main/java/com/twixer/api/security/WebSecurityConfig.java
+++ backend/api/src/main/java/com/twixer/api/security/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.twixer.api.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.twixer.api.security.jwt.AuthEntryPointJwt;
 import com.twixer.api.security.jwt.AuthTokenFilter;
 import com.twixer.api.security.services.UserDetailsServiceImpl;
 
@@ -21,6 +29,9 @@ import com.twixer.api.security.services.UserDetailsServiceImpl;
 public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDetailsServiceImpl userDetailsService;
+	
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
 
 	@Bean
 	AuthTokenFilter AuthTokenFilter() {
@@ -51,52 +62,35 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(
-						auth -> auth.requestMatchers("/api/v1/auth/**")
-								.permitAll().anyRequest().authenticated())
+						auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authenticationProvider(authenticationProvider())
+				.authenticationProvider(authenticationProvider()).cors(cors -> cors.configurationSource(corsConfigurationSource()))
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
 				.addFilterBefore(AuthTokenFilter(), UsernamePasswordAuthenticationFilter.class).build();
 	}
-	/*
-	 * @Bean SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	 * http.csrf(csrf -> csrf.disable()) // Deshabilitar CSRF
-	 * .exceptionHandling(exception ->
-	 * exception.authenticationEntryPoint(unauthorizedHandler)) // Manejo de //
-	 * excepciones .sessionManagement(session ->
-	 * session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin //
-	 * estado .authorizeHttpRequests(auth ->
-	 * auth.requestMatchers("/api/v1/auth/**").permitAll() // Permitir acceso a //
-	 * auth .requestMatchers("/api/v1/test/**").permitAll() // Permitir acceso a
-	 * test .anyRequest().authenticated()) // Autenticación para cualquier otra
-	 * solicitud .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-	 * // Configuración de CORS
-	 * 
-	 * http.authenticationProvider(authenticationProvider());
-	 * http.addFilterBefore(authenticationJwtTokenFilter(),
-	 * UsernamePasswordAuthenticationFilter.class);
-	 * 
-	 * return http.build(); }
-	 * 
-	 * // Configuración global de CORS
-	 * 
-	 * @Bean CorsFilter corsFilter() { UrlBasedCorsConfigurationSource source = new
-	 * UrlBasedCorsConfigurationSource(); CorsConfiguration config = new
-	 * CorsConfiguration(); config.setAllowCredentials(true);
-	 * config.setAllowedOrigins(Arrays.asList("http://localhost:5173",
-	 * "http://localhost:3000")); config.setAllowedHeaders(Arrays.asList("*"));
-	 * config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
-	 * "OPTIONS")); source.registerCorsConfiguration("/**", config); return new
-	 * CorsFilter(source); }
-	 * 
-	 * private CorsConfigurationSource corsConfigurationSource() { CorsConfiguration
-	 * configuration = new CorsConfiguration();
-	 * configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
-	 * "http://localhost:3000"));
-	 * configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE",
-	 * "OPTIONS")); configuration.setAllowCredentials(true);
-	 * configuration.setAllowedHeaders(Arrays.asList("*"));
-	 * UrlBasedCorsConfigurationSource source = new
-	 * UrlBasedCorsConfigurationSource(); source.registerCorsConfiguration("/**",
-	 * configuration); return source; }
-	 */
+
+
+	@Bean
+	CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+
+	private CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 }
